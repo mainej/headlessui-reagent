f4e23e736cfe855d819068721e838c5f6fad59eb
Jacob Maine <jacob.maine@gmail.com>
2021-04-20 17:11:53 -0700

    Add interop for Listbox

--- src/mainej/headlessui_reagent.cljs
+++ src/mainej/headlessui_reagent.cljs
@@ -7,6 +7,12 @@
 (def menu-items (utils/headlessui->reagent ui/Menu.Items))
 (def menu-item (utils/headlessui->reagent ui/Menu.Item))
 
+(def listbox (utils/headlessui->reagent ui/Listbox))
+(def listbox-button (utils/headlessui->reagent ui/Listbox.Button))
+(def listbox-label (utils/headlessui->reagent ui/Listbox.Label))
+(def listbox-options (utils/headlessui->reagent ui/Listbox.Options))
+(def listbox-option (utils/headlessui->reagent ui/Listbox.Option))
+
 (def disclosure (utils/headlessui->reagent ui/Disclosure))
 (def disclosure-button (utils/headlessui->reagent ui/Disclosure.Button))
 (def disclosure-panel (utils/headlessui->reagent ui/Disclosure.Panel))
